67b873f3c9c2fccf62a04bf98dbc4357c5f7122c
unknown <Allrentalfrank>
2022-10-25 23:09:30 +0800

    modify image.png

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+#include <math.h>
+int main()
+{
+	double a,xn=0,y1,y0;
+	scanf("%lf",&a);
+	y0=a;
+	for(int i=0;i<100000000;i++)
+	{
+		y1=y0*0.999;
+		xn+=0.001;
+		printf("xn=%lf\t yn=%lf\n",xn,y1);
+		if(y0-y1<=0.000001)
+		{
+		printf("the approximate answer is %lf",y1);
+		break;
+		}
+     	y0=y1;
+		
+	}
 	return 0;
+	
 }
-
